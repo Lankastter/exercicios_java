b08f97e3ede9e25e7a4d902ae6c2461800d90172
VICTOR LANKASTER <lankastersilva@gmail.com>
2023-08-29 18:08:38 -0300

    x

---
+++ src/Contag_Regress_While.java
@@ -0,0 +1,12 @@
+//Crindo contagem regressiva com o comando WHILE.
+
+public class Contag_Regress_While {
+    public static void main(String[]args){
+        int num = 1000;
+
+        while (num>0){
+            System.out.println(num);
+            num = num - 1;
+        }
+    }
+}

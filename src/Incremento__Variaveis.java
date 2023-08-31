0dcc42dc254f4ccd913e1d2d858f5e6e2de4e334
VICTOR LANKASTER <lankastersilva@gmail.com>
2023-08-31 19:35:48 -0300

    x

--- src/Incremento__Variaveis.java
+++ src/Incremento__Variaveis.java
@@ -6,8 +6,8 @@ public class Incremento__Variaveis{
         //fazendo, no caso abaixo, i valer 6 e j valer 5.
         int i = 5;
         int j = i++;
-        System.out.println("o valor de I será:"+i);
-        System.out.println("o valor de J será:"+j);
+        System.out.println("o valor de I será:"+i); // aqui I valera 6.
+        System.out.println("o valor de J será:"+j); // aqui J valera 5.
 
         System.out.println("-----------------------");
 
@@ -16,8 +16,8 @@ public class Incremento__Variaveis{
         //fazendo, no caso abaixo, a e b valerem 6 ambos.
         int a = 5;
         int b = ++a;
-        System.out.println("o valor de A será:"+a);
-        System.out.println("o valor de B será:"+b);
+        System.out.println("o valor de A será:"+a); // aqui A valera 6.
+        System.out.println("o valor de B será:"+b); // aqui B valera 6.
 
     }
 }

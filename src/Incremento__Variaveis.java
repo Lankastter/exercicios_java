c91d7fbd4de364a20bc800e67f2e3d4e7367c3bd
VICTOR LANKASTER <lankastersilva@gmail.com>
2023-08-31 19:31:39 -0300

    x

---
+++ src/Incremento__Variaveis.java
@@ -0,0 +1,23 @@
+public class Incremento__Variaveis{
+    public static void main(String[]args){
+
+        //Pós-Incremento
+        //O operador ++ , quando vem após a variável, retorna o valor antigo e o incrementa
+        //fazendo, no caso abaixo, i valer 6 e j valer 5.
+        int i = 5;
+        int j = i++;
+        System.out.println("o valor de I será:"+i);
+        System.out.println("o valor de J será:"+j);
+
+        System.out.println("-----------------------");
+
+        //Pré-Incremento
+        //O operador ++ , quando vem antes da variável, retorna o valor incrementado
+        //fazendo, no caso abaixo, a e b valerem 6 ambos.
+        int a = 5;
+        int b = ++a;
+        System.out.println("o valor de A será:"+a);
+        System.out.println("o valor de B será:"+b);
+
+    }
+}
